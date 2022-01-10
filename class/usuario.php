c459df7de2d89b641c9c8c95880bdd24384c9565
ruan <araujoruan456@gmail.com>
2022-01-10 16:09:29 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -57,7 +57,7 @@ class Usuario {
 
     public function loadById($id){
 
-        $sql = new sql();
+        $sql = new Sql();
 
         $results = $sql->select("SELECT * FROM tb_usuario WHERE idusuario = :ID", array(
 
@@ -74,6 +74,49 @@ class Usuario {
             $this->setDessenha($row['dessenha']);
             $this->setDtcadastro(new DateTime($row['dtcadastro']));
 
+        }
+
+    }
+
+    public static function getList(){
+       $sql = new Sql();
+
+       return $sql->select("SELECT * FROM tb_usuario ORDER BY deslogin");
+
+    }
+
+    public static function search($login){
+  
+    $sql = new Sql();
+
+    return $sql->select("SELECT *FROM tb_usuario where deslogin like :SEARCH ORDER BY deslogin",array(
+       'SEARCH'=>"%.$login.%"
+    ));
+
+    }
+
+    public function login($login , $password){
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN and dessenham = :PASSWORD ", array(
+
+            "LOGIN"=>$login,
+            "PASSWORD"=>$password
+
+        ));
+
+        if (count($results) > 0) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        } else{
+
+            throw new Exception("Login e/ou senha invalidos.");
         }
 
     }
